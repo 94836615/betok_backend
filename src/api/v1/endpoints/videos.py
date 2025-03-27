d888ca9c51191bc24e036cb2493cc7f7d6aef394
Noah <c94836615@gmail.com>
2025-03-28 00:59:02 +0100

    feat(vid-proc): made it possible to upload a video in to the blob storage, also implemented logger and did other small adjustments

--- src/api/v1/endpoints/videos.py
+++ src/api/v1/endpoints/videos.py
@@ -1,10 +1,28 @@
-from main import app
+from fastapi import APIRouter, UploadFile, File
+
+import logging
+
+from src.crud.video_upload import video_upload
+
+router = APIRouter()
+
+logger = logging.getLogger(__name__)
 
 
-@app.get("/videos")
-async def videos():
-    return {"data": "Videos list"}
+@router.get("/videos", status_code=200)
+def get_videos():
+    return "Videos"
 
-@app.post("/videos")
-async def create_video():
-    return {"data": "Video created successfully"}
+
+@router.post("/videos", status_code=201)
+async def post_video(video: UploadFile = File(...)):
+    logger.info("Video received:", video.filename)
+    contents = await video.read()
+    logger.info("File size:", len(contents))
+    if len(contents) == 0:
+        return {"error": "Bestand is leeg"}
+
+    # Reset file pointer
+    video.file.seek(0)
+
+    return await video_upload(video)
