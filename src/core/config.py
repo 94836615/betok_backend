d572cc92eeba4244cf0f72d479cee6fffd0137bf
Noah Nap <s1172260@student.windesheim.nl>
2025-04-01 11:40:45 +0200

    fix(api): fixed the fault where users cant upload their video

--- src/core/config.py
+++ src/core/config.py
@@ -1,17 +1,21 @@
 import os
+from urllib.parse import urlparse
 
 from dotenv import load_dotenv
 from minio import Minio
-
 load_dotenv()
 
 
 class Settings:
     prefix = os.getenv("PREFIX")
-    minio_client = Minio(os.environ.get("MINIO_API_LINK"),
+
+    minio_api_link = os.getenv("MINIO_API_LINK", "")
+    parsed_url = urlparse(minio_api_link)
+    minio_endpoint = parsed_url.netloc
+    minio_client = Minio(
+        minio_endpoint,
                          access_key=os.environ.get("MINIO_ACCESS_KEY"),
                          secret_key=os.environ.get("MINIO_SECRET_KEY"),
-                         secure=True)
-
+                         secure=(parsed_url.scheme == "https"))
 
 settings = Settings()
